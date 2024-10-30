1c918f42830991a9e10133b98393b14ad9ed13e8
savitar696 <karosen1448@gmail.com>
2024-10-30 16:02:11 +0300

    feat: initialization

---
+++ DllMain/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include "include.h"
+
+BOOL WINAPI testModule(HMODULE hModule) {
+	while (!GetAsyncKeyState(VK_END)) {
+		// do anything
+	}
+	FreeLibraryAndExitThread(hModule, 0);
+	return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD ul_reason_for_call,
+	LPVOID lpReserved)
+{
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hModule);
+		CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)testModule, 0, 0, 0));
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
---
+++ DllMain/include.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <Windows.h>
+#include <process.h>
+#include "offsets.h"
